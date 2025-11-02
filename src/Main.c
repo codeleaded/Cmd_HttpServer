85bf06f51748c888ab8d6b079a0d066641746a26
codeleaded <codeleaded@gmail.com>
2025-11-02 14:36:28 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -4,7 +4,7 @@ int main() {
     HttpServer hs = HttpServer_New(8080);
     if(hs.ok){
         HttpServer_Start(&hs);
-        Thread_Join(&hs.listener,NULL);
+        Thread_Join(&hs.listener);
     }
     HttpServer_Free(&hs);
 
